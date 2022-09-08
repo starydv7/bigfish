a1f0818ae11cc1d6d6c66b9db5b1805dbe40c552
starydv7 <pawanstar72926@gmail.com>
2022-09-09 00:26:00 +0530

    sequoia

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import Footer from './pages/Footer';
 import { Routes, Route } from "react-router-dom";
 import OurBusiness from './pages/OurBusiness';
 import Home from './pages/Home';
-
+import Services from './pages/Services';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element></Route>
+        <Route path="/" element={<Home/>}></Route>
         <Route path="/business" element={<OurBusiness/> }></Route>
         <Route path="/team" element></Route>
-        <Route path="/services" element></Route>
+        <Route path="/service" element={<Services/> }></Route>
       </Routes>
       <Footer/>
     </div>
--- src/pages/OurBusiness.jsx
+++ src/pages/OurBusiness.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import styles from "./our.module.css";
 const OurBusiness = () => {
   return (
-    <div>OurBusiness</div>
-  )
+    <div className={styles.cont}>
+      <div className={styles.num1}></div>
+      <div className={styles.num2}></div>
+      <div className={styles.num3}></div>
+      <div className={styles.num4}></div>
+      <div className={styles.num5}></div>
+      <div className={styles.num6}></div>
+      <div className={styles.num7}></div>
+      <div className={styles.num8}></div>
+      <div className={styles.num9}></div>
+      <div className={styles.num10}></div>
+      <div className={styles.num11}></div>
+      <div className={styles.num12}></div>
+      <div className={styles.num13}></div>
+    </div>
+  );
 }
 
 export default OurBusiness
