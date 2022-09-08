d3e939f6065947bfe51b86b91408dfa940a23452
starydv7 <pawanstar72926@gmail.com>
2022-09-09 01:27:05 +0530

    make align for data

--- src/pages/OurBusiness.jsx
+++ src/pages/OurBusiness.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import styles from "./our.module.css";
 const OurBusiness = () => {
   return (
-    <div className={styles.cont}>
-      <div className={styles.num1}></div>
-      <div className={styles.num2}></div>
-      <div className={styles.num3}></div>
-      <div className={styles.num4}></div>
-      <div className={styles.num5}></div>
-      <div className={styles.num6}></div>
-      <div className={styles.num7}></div>
-      <div className={styles.num8}></div>
-      <div className={styles.num9}></div>
-      <div className={styles.num10}></div>
-      <div className={styles.num11}></div>
-      <div className={styles.num12}></div>
-      <div className={styles.num13}></div>
-    </div>
+    <>
+      <div>
+        <h1>OUR BUSINESS</h1>
+      </div>
+      <div className={styles.cont}>
+        <div className={styles.num1}>slider</div>
+        <div className={styles.num2}>slider</div>
+        <div className={styles.num3}>IT</div>
+        <div className={styles.num4}>It-pics</div>
+        <div className={styles.num5}>Analytics</div>
+        <div className={styles.num6}>pics</div>
+        <div className={styles.num7}>construction</div>
+        <div className={styles.num8}>services-IT</div>
+        <div className={styles.num9}>More</div>
+        <div className={styles.num10}></div>
+        <div className={styles.num11}></div>
+        <div className={styles.num12}></div>
+        <div className={styles.num13}></div>
+      </div>
+    </>
   );
 }
 
